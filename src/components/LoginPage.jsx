497a68f82903c12673b84a1b3204978a002c6b6a
Sergey Kirillov <s_kirillov@rambler.ru>
2021-06-29 13:36:47 +0300

    trying authorization

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,6 +1,6 @@
-// import axios from 'axios';
+import axios from 'axios';
 import React, { useEffect, useRef, useState } from 'react';
-// import { useFormik } from 'formik';
+import { useFormik } from 'formik';
 import { Button, Form } from 'react-bootstrap';
 import { useLocation, useHistory } from 'react-router-dom';
 import useAuth from '../hooks/index.jsx';
@@ -8,19 +8,47 @@ import routes from '../routes.js';
 
 const LoginPage = () => {
 // BEGIN (write your solution here)
-  const handleSubmit = () => {
-    localStorage.setItem('key', '123');
-  };
+  const textInput = useRef();
+
+  const formik = useFormik({
+    initialValues: {
+      login: '',
+      password: '',
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+      // localStorage.setItem('key', '123');
+      // const host = 'http://localhost';
+      const sessionUrl = routes.loginPath();
+      console.log(sessionUrl);
+      console.log(values.login, values.password);
+      const auth = {
+        login: values.login,
+        password: values.passord,
+      };
+
+      axios.post(sessionUrl, {}, { auth })
+        .then(() => {
+          console.log('Authenticated');
+        }).catch(() => {
+          console.log('Error on Authentication');
+        });
+    },
+  });
+
+  useEffect(() => {
+    textInput.current.focus();
+  }, []);
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group controlId="username">
+    <Form onSubmit={formik.handleSubmit}>
+      <Form.Group controlId="login">
         <Form.Label>Username</Form.Label>
-        <Form.Control type="text" placeholder="username" />
+        <Form.Control type="text" placeholder="username" ref={textInput} onChange={formik.handleChange} value={formik.values.login} />
       </Form.Group>
       <Form.Group controlId="password">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="password" />
+        <Form.Control type="password" placeholder="password" onChange={formik.handleChange} value={formik.values.password} />
         <Form.Text className="invalid-feedback">
           the username or password is incorrect
         </Form.Text>
